fceaaabecb1e1089ae4ce8c82735fca674d74f1d
Nikita <gora_mysora@inbox.ru>
2024-01-10 20:29:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.button import  Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        return sm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
